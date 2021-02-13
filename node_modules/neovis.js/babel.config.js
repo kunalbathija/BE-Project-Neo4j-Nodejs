41f19af9d771f3f1fc37cc4438fd885f8161feb7
Kunal <kunalbathija97@gmail.com>
2021-02-13 18:21:42 +0530

    removed node_modules

--- node_modules/neovis.js/babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	"presets": [["@babel/preset-env", {}]],
-	"plugins": [
-		["@babel/plugin-transform-runtime", {"corejs": 3}],
-		"@babel/plugin-proposal-class-properties"
-	]
-}
